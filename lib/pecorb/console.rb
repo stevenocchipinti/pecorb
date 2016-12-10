270a73c23deb1a9dd37120aed03de9ea2d1cb23d
Steven Occhipinti <dev@stevenocchipinti.com>
2016-12-10 16:13:55 +1100

    Refactor and allow the streams to be configured
    The main reason for allowing the streams to be configured is that when
    this tool is used on the command line as a CLI utility, it's important
    that the prompts are displayed via STDERR and the final result is
    displayed via STDOUT to allow the tool to be used in a pipline, for
    example:
    
    ```
    ls | pecorb | xargs cat
    ```
    
    However, when used as a library as part of an existing ruby application,
    the application should be able to choose which stream is used for
    prompts as normally it should just be STDOUT and reserve STDERR for
    _diagnostics_ and _errors_.

--- lib/pecorb/console.rb
+++ lib/pecorb/console.rb
@@ -9,51 +9,60 @@ module Pecorb
     LEFT  = "#{CSI}D"
 
     def read_char
-      $stdin.echo = false
-      $stdin.raw!
-      input = $stdin.getc.chr
+      input_stream.echo = false
+      input_stream.raw!
+      input = input_stream.getc.chr
       if input == "\e" then
-        input << $stdin.read_nonblock(3) rescue nil
-        input << $stdin.read_nonblock(2) rescue nil
+        input << input_stream.read_nonblock(3) rescue nil
+        input << input_stream.read_nonblock(2) rescue nil
       end
     ensure
-      $stdin.echo = true
-      $stdin.cooked!
+      input_stream.echo = true
+      input_stream.cooked!
       return input
     end
 
-    def print(val)          $stderr.print val                 end
-    def puts(val)           $stderr.puts val                  end
-    def output(val)         $stdout.puts val                  end
+    def puts(val)           output_stream.puts val                  end
+    def print(val)          output_stream.print val                 end
 
-    def up(n=1)             $stderr.print "#{CSI}#{n}A"       end
-    def down(n=1)           $stderr.print "#{CSI}#{n}B"       end
-    def right(n=1)          $stderr.print "#{CSI}#{n}C"       end
-    def left(n=1)           $stderr.print "#{CSI}#{n}D"       end
+    def up(n=1)             output_stream.print "#{CSI}#{n}A"       end
+    def down(n=1)           output_stream.print "#{CSI}#{n}B"       end
+    def right(n=1)          output_stream.print "#{CSI}#{n}C"       end
+    def left(n=1)           output_stream.print "#{CSI}#{n}D"       end
 
-    def backspace(n=1)      $stderr.print "\b"*n              end
-    def carriage_return()   $stderr.print "\r"                end
-    def clear_to_eol()      $stderr.print "#{CSI}K"           end
-    def clear_to_eos()      $stderr.print "#{CSI}J"           end
-    def clear_screen()      $stderr.print "#{CSI}H#{CSI}J"    end
+    def backspace(n=1)      output_stream.print "\b"*n              end
+    def carriage_return()   output_stream.print "\r"                end
+    def clear_to_eol()      output_stream.print "#{CSI}K"           end
+    def clear_to_eos()      output_stream.print "#{CSI}J"           end
+    def clear_screen()      output_stream.print "#{CSI}H#{CSI}J"    end
 
-    def black()             $stderr.print "#{CSI}#{CSI}30m"   end
-    def red()               $stderr.print "#{CSI}#{CSI}31m"   end
-    def green()             $stderr.print "#{CSI}#{CSI}32m"   end
-    def yellow()            $stderr.print "#{CSI}#{CSI}33m"   end
-    def blue()              $stderr.print "#{CSI}#{CSI}34m"   end
-    def magenta()           $stderr.print "#{CSI}#{CSI}35m"   end
-    def cyan()              $stderr.print "#{CSI}#{CSI}36m"   end
-    def white()             $stderr.print "#{CSI}#{CSI}37m"   end
-    def reset_color()       $stderr.print "#{CSI}#{CSI}0m"    end
+    def black()             output_stream.print "#{CSI}#{CSI}30m"   end
+    def red()               output_stream.print "#{CSI}#{CSI}31m"   end
+    def green()             output_stream.print "#{CSI}#{CSI}32m"   end
+    def yellow()            output_stream.print "#{CSI}#{CSI}33m"   end
+    def blue()              output_stream.print "#{CSI}#{CSI}34m"   end
+    def magenta()           output_stream.print "#{CSI}#{CSI}35m"   end
+    def cyan()              output_stream.print "#{CSI}#{CSI}36m"   end
+    def white()             output_stream.print "#{CSI}#{CSI}37m"   end
+    def reset_color()       output_stream.print "#{CSI}#{CSI}0m"    end
 
     def save_pos
-      $stderr.print "#{CSI}s"
+      output_stream.print "#{CSI}s"
       if block_given?
         yield
         load_pos
       end
     end
-    def load_pos() $stderr.print "#{CSI}u" end
+    def load_pos() output_stream.print "#{CSI}u" end
+
+    private
+
+    def input_stream
+      Pecorb.config.input_stream
+    end
+
+    def output_stream
+      Pecorb.config.output_stream
+    end
   end
 end
---
+++ lib/pecorb/list.rb
@@ -0,0 +1,137 @@
+require_relative "console"
+
+module Pecorb
+  module List
+    extend self
+    extend Console
+
+    @input = ""
+    @cursor = 0
+    @selected = 0
+    @items = []
+    @displayed_items = []
+
+
+    def prompt(items, prompt="Select an item: ")
+      return unless items.is_a? Enumerable
+      @displayed_items = @items = items
+      @prompt = prompt
+
+      print_menu
+
+      while c = read_char
+        case c
+        when "", "\r"
+          break
+        when ""
+          carriage_return
+          clear_to_eos
+          exit 0
+        when ""
+          clear_screen
+          print_menu
+        when "" # Backspace key
+          next if @input.empty? || @cursor <= 0
+          @input.slice!(@cursor-1)
+          replace_input(@input)
+          replace_items { filter_items(@items, @input) }
+          backspace
+          @cursor -= 1
+        when Console::LEFT
+          next unless @cursor > 0
+          print c
+          @cursor -= 1
+        when Console::RIGHT
+          next unless @cursor < @input.length
+          print c
+          @cursor += 1
+        when Console::UP, ""
+          @selected = (@selected - 1) % @displayed_items.size
+          replace_items { filter_items(@items, @input) }
+        when Console::DOWN, "\n" # CTRL-J enters a linefeed char in bash
+          @selected = (@selected + 1) % @displayed_items.size
+          replace_items { filter_items(@items, @input) }
+        else
+          @input.insert(@cursor, c)
+          replace_input(@input)
+          replace_items { filter_items(@items, @input) }
+          print c
+          @cursor += 1
+        end
+      end
+
+      backspace(@cursor)
+      clear_to_eos
+      cyan
+      puts @displayed_items[@selected]
+      reset_color
+      @displayed_items[@selected]
+    end
+
+    private
+
+    def print_menu
+      puts @prompt
+      print_items(@displayed_items)
+      move_cursor_from_end_to_start
+      print @input
+    end
+
+    def move_cursor_from_end_to_start
+      up(@displayed_items.size + 1)
+      right(@prompt.size)
+    end
+
+    def move_cursor_from_start_to_end
+      down(@displayed_items.size + 1)
+      carriage_return
+    end
+
+    def replace_input(str)
+      save_pos do
+        backspace(@cursor)
+        print @input
+        clear_to_eol
+      end
+    end
+
+    def replace_items
+      return unless block_given?
+      list_size = @displayed_items.size
+      save_pos do
+        down
+        carriage_return
+        clear_to_eol
+        if list_size > 0
+          (list_size - 1).times { down; clear_to_eol}
+          (list_size - 1).times { up }
+        end
+        @displayed_items = yield
+        @selected = limit_max @selected, list_size - 1
+        print_items @displayed_items
+      end
+    end
+
+    def limit_max(n, max)
+      [[max, n].min, 0].max
+    end
+
+    def print_items(items)
+      items.each_with_index do |item, i|
+        if @selected == i
+          cyan
+          print "‣ "
+        else
+          print "  "
+        end
+        puts item
+        reset_color
+      end
+    end
+
+    def filter_items(items, filter)
+      regex = Regexp.new(filter.chars.join(".*"), "i")
+      items.select {|i| regex.match i }
+    end
+  end
+end
